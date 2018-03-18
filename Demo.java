42010ad655de53a74d1355df190d37540379ccc6
liuky-ape <34020226+liuky-ape@users.noreply.github.com>
2018-03-18 15:30:06 +0800

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+package ｅｉｇｈｔ＿ｑｕｅｅｎｓ;
+
+public class Demo {
+    public static void main(String[] args) {
+        EightQueens et=new EightQueens();
+        et.settleQueen(0);
+        et.printResult();
+    }
+}
